ea7b9bb832ebf7cc4e88e4490fb70f114e5eda6f
Javaispasgithupmaisbonpasgrave <vaillant.milly@gmail.com>
2016-10-14 10:57:58 +0200

    Create Plan

---
+++ FastDelivery/FastDelivery_Library/Plan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDelivery_Library
+{
+    public class Plan
+    {
+        public Dictionary<int,Point> ListPoint;
+        public Dictionary<int,Troncon> ListTroncon;
+        public Tournee tournee;
+        public DemandeLivraison livAsk;
+
+        public Plan(Dictionary<int, Point> HashPoint, Dictionary<int, Troncon> HashTroncon,Tournee tourneet, DemandeLivraison demande)
+        {
+            this.ListPoint = HashPoint;
+            this.ListTroncon = HashTroncon;
+            this.tournee = tourneet;
+            this.livAsk = demande;
+
+        }
+    }
+}
