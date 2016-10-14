967c59f439c4eb5963ec83206e10dad83dee1e13
Javaispasgithupmaisbonpasgrave <vaillant.milly@gmail.com>
2016-10-14 10:59:41 +0200

    Update Demande / Tournee

---
+++ FastDelivery/FastDelivery_Library/DemandeLivraison.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDelivery_Library
+{
+    public class DemandeLivraison
+    {
+        public Dictionary<int, Livraison> HashLiv;
+
+        public DemandeLivraison(Dictionary<int,Livraison> hashliv)
+        {
+            this.HashLiv = hashliv;
+        }
+
+    }
+}
---
+++ FastDelivery/FastDelivery_Library/Tournee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDelivery_Library
+{
+    public class Tournee
+    {
+        public List<Troncon> ListTroncon;
+        public List<Livraison> ListLivraison;
+
+        public Tournee(List<Troncon> TroncList,List<Livraison> LivraisonList)
+        {
+            this.ListTroncon = TroncList;
+            this.ListLivraison = LivraisonList;
+        }
+    }
+}
