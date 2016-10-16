b0b6f2d0ebd3d10400aa71337e2675fa703987ad
Javaispasgithupmaisbonpasgrave <vaillant.milly@gmail.com>
2016-10-16 15:57:13 +0200

    Update TSP

---
+++ FastDelivery/FastDelivery_Library/TSP1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace FastDelivery_Library
+{
+
+    public class TSP1 : TemplateTSP
+    {
+        override protected IIterator<int> iterator(int sommetCrt, List<int> nonVus, int[][] cout, int[] duree)
+    {
+        return new IteratorSeq(nonVus, sommetCrt);
+    }
+
+        override protected int bound(int sommetCourant, List<int> nonVus, int[][] cout, int[] duree)
+    {
+        return 0;
+    }
+}
+
+}
