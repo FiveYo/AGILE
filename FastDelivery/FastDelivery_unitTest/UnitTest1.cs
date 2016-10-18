69f62b95d8b2309dbb0b498c93f59fe576ef0bc4
Mathieu <mathieu.rollet@insa-lyon.fr>
2016-10-18 10:38:44 +0200

    Add TestParserXml_Livraison()

--- FastDelivery/FastDelivery_unitTest/UnitTest1.cs
+++ FastDelivery/FastDelivery_unitTest/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastDelivery_Library;
+using System.Collections.Generic;
 
 namespace FastDelivery_unitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UniTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestParserXml_Livraison()
         {
+            FileStream planFile = new FileStream(@"..\..\..\..\XMLExample\plan5x5.xml", FileMode.Open, FileAccess.Read);
+            FileStream livraisonFile = new FileStream(@"..\..\..\..\XMLExample\livraison5x5-4.xml", FileMode.Open, FileAccess.Read);
+            StructPlan structPlan = Outils.ParserXml_Plan(planFile);
+            StructLivraison livRetournee = Outils.ParserXml_Livraison(livraisonFile, structPlan.HashPoint);
+            StructLivraison livAttendu = new StructLivraison();
         }
     }
 }
