151e31020fd20207ed917937393f3ffc0b531937
Javaispasgithupmaisbonpasgrave <vaillant.milly@gmail.com>
2016-11-04 14:37:12 +0100

    Update UNdoredo

--- FastDelivery/FastDelivery_Library/PatternUndoRedo/UndoRedo.cs
+++ FastDelivery/FastDelivery_Library/PatternUndoRedo/UndoRedo.cs
@@ -24,12 +24,12 @@ namespace FastDelivery_Library.PatternUndoRedo
                 if (UndoStruct.action==ActionType.supprimer)
                 {
                     tournee.AddLivraison(UndoStruct.carte,UndoStruct.livraison,UndoStruct.index);
-                    // add RedoPushUndoDelete
+                    RedoPushInUnDoForDelete(UndoStruct.carte, UndoStruct.livraison, UndoStruct.index);
                 }
                 else if (UndoStruct.action==ActionType.ajouter)
                 {
                     tournee.DelLivraison(UndoStruct.carte, UndoStruct.livraison, UndoStruct.index);
-                    // add RedoPushUndoAdd
+                    RedoPushInUnDoForAdd(UndoStruct.carte, UndoStruct.livraison, UndoStruct.index);
                 }
                 else if (UndoStruct.action==ActionType.modifier)
                 {
@@ -49,13 +49,17 @@ namespace FastDelivery_Library.PatternUndoRedo
 
                 if (RedoStruct.action == ActionType.supprimer)
                 {
+                    
+                    ModifyTournee ChangeforDelete = this.MakeChangeObjectForDelete(RedoStruct.carte, RedoStruct.livraison, RedoStruct.index);
+                    _UndoActionsContainer.Push(ChangeforDelete);
                     tournee.DelLivraison(RedoStruct.carte, RedoStruct.livraison, RedoStruct.index);
-                    // add RedoPushUndoDelete
+
                 }
                 else if (RedoStruct.action == ActionType.ajouter)
                 {
+                    ModifyTournee ChangeforAdd = this.MakeChangeObjectForAdd(RedoStruct.carte, RedoStruct.livraison, RedoStruct.index);
+                    _UndoActionsContainer.Push(ChangeforAdd);
                     tournee.AddLivraison(RedoStruct.carte, RedoStruct.livraison, RedoStruct.index);
-                    // add RedoPushUndoAdd
                 }
                 else if (RedoStruct.action == ActionType.modifier)
                 {
@@ -66,5 +70,83 @@ namespace FastDelivery_Library.PatternUndoRedo
             }
 
         }
+
+        public ModifyTournee MakeChangeObjectForAdd(Modele.Carte carte, Livraison livraison, int index)
+        {
+            ModifyTournee UndoStruct = new ModifyTournee();
+            UndoStruct.index = index;
+            UndoStruct.carte = carte;
+            UndoStruct.livraison = livraison;
+            return UndoStruct;
+        }
+        public ModifyTournee MakeChangeObjectForDelete(Modele.Carte carte, Livraison livraison, int index)
+        {
+            ModifyTournee UndoStruct = new ModifyTournee();
+            UndoStruct.index = index;
+            UndoStruct.carte = carte;
+            UndoStruct.livraison = livraison;
+            return UndoStruct;
+        }
+        public ModifyTournee MakeChangeObjectForModify(Modele.Carte carte, Livraison livraison, int index)
+        {
+            //TODO
+            ModifyTournee UndoStruct = new ModifyTournee();
+            return UndoStruct;
+            
+        }
+        public void RedoPushInUnDoForAdd(Modele.Carte carte, Livraison livraison, int index)
+        {
+            ModifyTournee RedoStruct = new ModifyTournee();
+            RedoStruct.action = ActionType.ajouter;
+            RedoStruct.carte = carte;
+            RedoStruct.livraison = livraison;
+            RedoStruct.index = index;
+            _RedoActionsContainer.Push(RedoStruct);
+        }
+        public void RedoPushInUnDoForDelete(Modele.Carte carte, Livraison livraison, int index)
+        {
+            ModifyTournee RedoStruct = new ModifyTournee();
+            RedoStruct.action = ActionType.supprimer;
+            RedoStruct.carte = carte;
+            RedoStruct.livraison = livraison;
+            RedoStruct.index = index;
+            _RedoActionsContainer.Push(RedoStruct);
+
+        }
+        public void RedoPushInUnDoForModify(Modele.Carte carte, Livraison livraison, int index)
+        {
+            //TODO
+
+        }
+
+        public void clearUnRedo()
+        {
+            _UndoActionsContainer.Clear();
+        }
+        public bool IsUndoPossible()
+        {
+            if (_UndoActionsContainer.Count != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool IsRedoPossible()
+        {
+            if (_RedoActionsContainer.Count != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
