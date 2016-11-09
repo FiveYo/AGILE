172381b46019ce95e0ed6bdaa92cd5c42be48574
stoko973 <stoko973>
2016-11-09 23:10:32 +0100

    Ajout NotifyProperty dans livraison

--- FastDelivery/FastDelivery_Library/Modele/Livraison.cs
+++ FastDelivery/FastDelivery_Library/Modele/Livraison.cs
@@ -1,7 +1,9 @@
 ﻿using FastDelivery_Library.Modele;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +12,58 @@ namespace FastDelivery_Library.Modele
     /// <summary>
     /// Contient la durée, l'adresse d'une livraison. Si celle-ci est planifié contient la plage horaire
     /// </summary>
-    public class Livraison : Lieu
+    public class Livraison : Lieu, INotifyPropertyChanged
     {
         public int duree;
 
-        public DateTime debutPlage { get; set; }
-        public DateTime finPlage { get; set; }
+        public DateTime debutPlage {
+            get
+            {
+                return this.debutPlage;
+            }
+            set
+            {
+                if (value != this.debutPlage)
+                {
+                    this.debutPlage = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public DateTime finPlage
+        {
+            get
+            {
+                return this.finPlage;
+            }
+            set
+            {
+                if (value != this.finPlage)
+                {
+                    this.finPlage = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
         public bool planifier { get; set; }
         public Point adresse { get; set; }
 
         public DateTime HeureDePassage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         public Livraison(Point adresse , int duree)
         {
             planifier = false;
