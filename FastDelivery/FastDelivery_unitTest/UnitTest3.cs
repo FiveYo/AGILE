64d4c70bbc37280198e57bbed963f65f7d0466d5
jkancel971 <justin.kancel@insa-lyon.fr>
2016-11-10 22:21:40 +0100

    yayayayayyaa

---
+++ FastDelivery/FastDelivery_unitTest/UnitTest3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FastDelivery_unitTest
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
